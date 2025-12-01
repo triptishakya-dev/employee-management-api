1ad1bf4891c38ef92b88f784cf29622c462f1ace
triptishakya-dev <triptishakya.2001@gmail.com>
2025-12-02 01:31:45 +0530

    feat: Implement employee creation and retrieval API endpoints with validation.

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -26,6 +26,29 @@ export const postEmployee = async (req, res) => {
       return res.status(400).json({ error: "All fields are required" });
     }
 
+    // salary validation - salary should be greter then 0
+    if (salary <= 0) {
+      return res.status(400).json({ error: "Salary should be greater than 0" });
+    }
+    console.log("joiningDate", joiningDate)
+    console.log("joiningDate", new Date(joiningDate))
+    // joining date validation - joining date should not be past
+    if (new Date(joiningDate) < new Date()) {
+      return res.status(400).json({ error: "Joining date should not be past" });
+    
+    }
+      // duplicate email validation 
+      const existingEmployee = await prisma.employee.findUnique({
+        where: {
+          officialEmail: officialEmail,
+        },
+      });
+      if (existingEmployee) 
+        return res.status(400).json({ error: "Employee with this email already exists" });
+      }
+    }
+
+
     const newEmployee = await prisma.employee.create({
       data: {
         empCode,
@@ -54,7 +77,7 @@ export const getEmployees = async (req, res) => {
   try {
     const employees = await prisma.employee.findMany();
     console.log(employees)
-    res.status(200).json({message: "Employees fetched successfully" , employees });
+    res.status(200).json({ message: "Employees fetched successfully", employees });
   } catch (error) {
     console.error("Error fetching employees:", error);
     res.status(500).json({ error: "Internal server error" });
