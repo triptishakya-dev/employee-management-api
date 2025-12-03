efa6cd940b1b34f9159717def7503254d847077a
triptishakya-dev <triptishakya.2001@gmail.com>
2025-12-03 05:59:44 +0530

    feat: Add address routes and controller for creating and fetching employee addresses.

--- routers/addressRoute.js
+++ routers/addressRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { postAddress } from "../controller/addressController.js";
+import { getAddresses, postAddress } from "../controller/addressController.js";
 
 const router = express.Router();
 
 router.post("/api/addresses", postAddress);
+router.get("/api/addresses", getAddresses);
 
 export default router;
